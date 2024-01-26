718f983ad7fb5511e3d546e9ee6ba5105a5e6e9d
Gabe <gabriel.velasquez0116@gmail.com>
2024-01-26 14:03:22 -0800

    Initial Commit

---
+++ Meal-Select-Code-Exercise/Meal_Select_Code_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_Select_Code_ExerciseApp.swift
+//  Meal-Select-Code-Exercise
+//
+//  Created by Gabriel  Velasquez on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Meal_Select_Code_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
