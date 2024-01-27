7aaa21323d4243f0a172b8b68f546061b5f50b82
Gabe <gabriel.velasquez0116@gmail.com>
2024-01-26 16:52:09 -0800

    Implementing dropdown choice for user to select. Going to have it do an api call based on selected item

--- Meal-Select-Code-Exercise/ContentView.swift
+++ Meal-Select-Code-Exercise/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedOption = 0
+        let options = [
+            "Beef",
+            "Breakfast",
+            "Chicken",
+            "Goat",
+            "Lamb",
+            "Miscellaneous",
+            "Pasta",
+            "Pork",
+            "Seafood",
+            "Side",
+            "Starter",
+            "Vegan",
+            "Vegetarian"
+        ]
+    @State private var meals: [Meal] = []
+    
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What sounds good to eat?")
+                .bold()
+                .padding(10)
+            Picker(selection: $selectedOption, label: Text("Select an option")) {
+                            ForEach(0..<options.count) { index in
+                                Text(self.options[index]).tag(index)
+                            }
+                        }
+                        .pickerStyle(MenuPickerStyle()) // Apply MenuPickerStyle to make it look like a dropdown menu
+
+                        Text("Selected Option: \(options[selectedOption])")
+                            .padding()
         }
-        .padding()
+//        NavigationView{
+//            List{
+//                ForEach(viewModel.meals, id:\.self){ meal in
+//                    HStack{
+//                        Image("")
+//                            .frame(width:120, height:55)
+//                            .background(Color.gray)
+//                        Text(meal.name)
+//                            .bold()
+//                    }
+//                }
+//            }
+//            .navigationTitle("Meals")
+//            .onAppear(){
+//                viewModel.fetch()
+//            }
+//        }
+        
+        Spacer()
     }
 }
 
---
+++ Meal-Select-Code-Exercise/Meals.swift
@@ -0,0 +1,50 @@
+//
+//  Meals.swift
+//  Meal-Select-Code-Exercise
+//
+//  Created by Gabriel  Velasquez on 1/26/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Meal: Hashable, Codable{
+        let idMeal: Int
+        let strMeal: String
+        let strCategory: String
+        let strArea: String
+        let strInstructions: String
+        let strMealThumb: String
+        let strTags: String
+        let strYoutube: String
+        let strIngredient: String
+        let strMeasure: String
+}
+
+class ViewModel: ObservableObject{
+    @Published var meals: [Meal] = []
+    
+    func fetch(){
+        guard let url = URL(string:
+            "www.themealdb.com/api/json/v1/1/lookup.php?i=52772")else{
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url){ [weak self] data, _,
+            error in
+            guard let data=data,error==nil else{
+                return
+            }
+            do{
+                let meals=try JSONDecoder().decode([Meal].self,
+                    from:data)
+                DispatchQueue.main.async{
+                    self?.meals = meals
+                }
+            }catch{
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
+
